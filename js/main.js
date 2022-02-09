a048eb67a6d8fc1c795fc5eeda48f6b6f855ca3b
Sor-Uddin-Shanto <soruddinshanto@gmail.com>
2022-02-09 13:45:40 +0600

    push02

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ let pullFollow = document.getElementById("pull-follow");
 followBtn.addEventListener("click", folllowAreaShow);
 pullFollow.addEventListener("click", folllowAreaShow);
 
-function folllowAreaShow(){
+function folllowAreaShow() {
     followIcons.style.transform = "translateX(0px)";
     pullFollow.style.opacity = "0.7"
 };
@@ -32,16 +32,16 @@ window.addEventListener("scroll", () => {
     let sectionPos = skillsSection.getBoundingClientRect().top;
     let sereenPos = window.innerHeight / 1.5;
 
-    if(sectionPos < sereenPos){
-        $('.skill-per').each(function(){
+    if (sectionPos < sereenPos) {
+        $('.skill-per').each(function () {
             var $this = $(this);
             var per = $this.attr('per');
-            $this.css("width",per+'%');
+            $this.css("width", per + '%');
         });
-    }else{
-        $('.skill-per').each(function(){
+    } else {
+        $('.skill-per').each(function () {
             var $this = $(this);
-            $this.css("width",0+'%');
+            $this.css("width", 0 + '%');
         });
     }
 });
@@ -51,3 +51,107 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+
+
+// The rotating circle script
+
+$(document).ready(function () {
+    var entries = [{
+            label: 'HTML'
+        },
+        {
+            label: 'CSS'
+        },
+        {
+            label: 'JAVASCRIPT'
+        },
+        {
+            label: 'JQUERY'
+        },
+        {
+            label: 'BOOTSTRAP'
+        },
+        {
+            label: 'SASS'
+        },
+        {
+            label: 'REACT JS'
+        },
+        {
+            label: 'NODE JS'
+        },
+        {
+            label: 'MONGODB'
+        },
+        {
+            label: 'PHP'
+        },
+        {
+            label: 'WORDPRESS'
+        },
+        {
+            label: 'LARAVEL'
+        },
+        {
+            label: 'ANGULAR'
+        },
+        {
+            label: 'VUE JS'
+        },
+        {
+            label: 'GIT'
+        },
+        {
+            label: 'GITHUB'
+        },
+        {
+            label: 'SEO'
+        },
+        {
+            label: 'EXPRESS JS'
+        },
+        {
+            label: 'AJAX'
+        },
+        {
+            label: 'SYMFONY'
+        },
+        {
+            label: 'EMBER'
+        },
+        {
+            label: 'JSON'
+        },
+        {
+            label: 'MYSQL'
+        },
+        {
+            label: 'BACKBONE JS'
+        }
+    ];
+
+    var settings = {
+        entries: entries,
+        width: 440,
+        height: 380,
+        radius: '65%',
+        radiusMin: 75,
+        bgDraw: true,
+        bgColor: '#040f2f',
+        opacityOver: 1.00,
+        opacityOut: 0.05,
+        opacitySpeed: 6,
+        fov: 800,
+        speed: 1.2,
+        fontFamily: 'Courier, Arial, sans-serif',
+        fontSize: '28',
+        fontColor: '#fff',
+        fontWeight: 'bold',
+        fontStyle: 'normal',
+        fontSretch: 'normal',
+        fontToUpperCase: true,
+
+    };
+    $('#the-circle').svg3DTagCloud(settings);
+});
